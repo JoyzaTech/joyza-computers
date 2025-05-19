baa554880ead747583662e5b2b91318f25dc58b9
conner12345678 <cehly065@west-mec.org>
2025-05-19 14:36:46 -0700

    adding map

--- app/page.js
+++ app/page.js
@@ -1,9 +1,16 @@
-
+import BriefInfo from '../components/briefInfo'
+import { computers } from '../data'
 
 export default function Home() {
 
 
   return (
-    <p>hi</p>
+    <div>
+      {Object.values(computers).map((item, index) => {
+        return(
+          <BriefInfo key={item.name} img={item.images[0]} title={item.name} />
+        )
+      })}
+    </div>
   )
 }
