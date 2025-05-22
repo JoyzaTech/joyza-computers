5b8e0bdbe809bec8fd9210fb91ebf49386b9a060
Diego-Esquivias <desqui048@west-mec.org>
2025-05-21 23:12:56 -0700

    Slight style and function change
    - X was more noticeable
    - Made it so when you add something to cart it automatically closes the popup

--- components/cart.jsx
+++ components/cart.jsx
@@ -3,7 +3,7 @@ export default function Cart({ items, prices, quantitys, onClose }) {
         <>
             <div className="fixed inset-0 bg-black bg-opacity-10 z--1 opacity-60"></div>
             <div className="bg-[#3A3A3A] shadow-lg max-w-md w-full relative flex flex-col items-center text-center m-3 p-3 rounded-xl">
-                <button className="absolute top-2 right-2 text-gray-600 hover:text-gray-900" onClick={onClose}>X</button>
+                <button className="absolute top-2 right-2 text-white text-xl hover:text-gray-400" onClick={onClose}>X</button>
                 {items.length > 0 ? (
                     items.map((item, index) => {
                         return(
--- components/popup.jsx
+++ components/popup.jsx
@@ -28,12 +28,20 @@ export default function Popup({ info, onClick, onAdd }) {
                         <button onClick={() => quantity > 0 ? setQuantity(quantity - 1) : setQuantity(quantity)} className="m-2"><CgMathMinus /></button>
                         <div className="bg-[#5D5D5D] w-[2vw] h-[2vw] rounded-full pt-1 text-white my-2"><p>{quantity}</p></div>
                         <button onClick={() => setQuantity(quantity + 1)} className="m-2"><CgMathPlus /></button>
-                        <button className="bg-[#0071C7] hover:bg-[#005FA8] p-2 rounded-lg" onClick={() => onAdd(info.name, info.price, quantity)}>Add to cart</button>
+                        <button
+                        className="bg-[#0071C7] hover:bg-[#005FA8] p-2 rounded-lg"
+                        onClick={() => {
+                        onAdd(info.name, info.price, quantity);
+                        onClick(); // This will close the popup
+                        }}
+                        >
+                        Add to cart
+                        </button>
                     </div>
                 </div>
 
                 <div className="bg-[#3A3A3A] shadow-lg max-w-md w-full relative flex flex-col items-center text-center m-3 p-3 rounded-xl">
-                    <button className="absolute top-2 right-2 text-gray-600 hover:text-gray-900" onClick={onClick}>X</button>
+                    <button className="absolute top-2 right-2 text-white text-xl hover:text-gray-400" onClick={onClick}>X</button>
                     <h1 className="text-[2.5vw] p-3 font-bold text-[#0071C7]">Description: </h1>
                     <p className="text-[1.5vw]">{info.description}</p>
                     <hr className="bg-[#5D5D5D] w-full h-[4px] border-none my-4"/>
